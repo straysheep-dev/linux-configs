bbbbbdb05913bcd5954802325428e85c69d1ae52
straysheep-dev <84547874+straysheep-dev@users.noreply.github.com>
2021-08-18 18:26:47 -0400

    Added cron functionality, improved error handling
    Still needs to backup and restore the current "OK" conf file during root/cron in case something goes wrong with unbound.

--- unbound-update-blocklist.sh
+++ unbound-update-blocklist.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
 # Prepare the latest blocklist and check for parsing errors
-# Requires manual review before installing, can be changed to run as root via cron and install automatically
+# Run as root via cron to install automatically or run as normal user to check manually before installing
 
 # Crontab to run every 12 hours:
 # 0 */12 * * * /bin/bash /opt/unbound-update-blocklist.sh
 # Do not run more often than once every 5 minutes: <https://urlhaus.abuse.ch/api/#tos>
 
+UID1000=$(grep '1000' /etc/passwd | cut -d ':' -f 1)
+
 UPDATE_DIR=$(mktemp -d)
 export UPDATE_DIR
 
 # Make a temporary working directory to work with these files alone
-cd "$UPDATE_DIR" || (echo "Failed to make temporary directory. Quitting." && touch ~/directory-creation.error && exit)
+cd "$UPDATE_DIR" || (echo "Failed to make temporary directory. Quitting." && touch /home/"$UID1000"/directory-creation.error && exit)
 
 # Download blocklists
 curl -sS 'https://urlhaus.abuse.ch/downloads/hostfile/' > 'urlhaus.txt'
 curl -sSLf 'https://raw.githubusercontent.com/StevenBlack/hosts/master/data/yoyo.org/hosts' > 'yoyo.txt'
 
-# Make sure download was successful and parse hosts format to unbound.conf format
-if ! (file "${UPDATE_DIR}/*.txt" | grep -qx "^.* empty$"); then
-	cat ./*.txt | sed '/^$/d' | sed 's/\r//g' | sed '/^#/!s/^0.0.0.0[[:space:]]//g' | sed '/^#/!s/^127.0.0.1[[:space:]]//g' | sed '/^#/!s/$[[:space:]]//g' | sed '/^#/!s/^/local-zone: "/g' | sed '/^#/!s/$/" always_nxdomain/g' > "$(date +%Y-%m-%d_%H:%M:%S_)"blocklist.conf
-else
-	(echo "One or more lists empty. Quitting." && rm -rf "$UPDATE_DIR" && touch ~/blocklist-download.error && exit)
-fi
+# Make sure download was successful and filetype matches what's expected
+for list in ./*.txt; do
+	if ! (file "$list" | grep -Eqx "^./.*\.txt:.*(ASCII text|ASCII text, with CRLF line terminators)$"); then
+		(echo "One or more lists not plain text. Quitting." && touch /home/"$UID1000"/blocklist-download.error && cd ~/ && rm -rf "$UPDATE_DIR")
+		exit
+	fi
+done
+
+# Parse hosts files to unbound.conf format; handles removing CRLF line terminators and empty lines
+cat ./*.txt | sed '/^$/d' | sed 's/\r//g' | sed '/^#/!s/^0.0.0.0[[:space:]]//g' | sed '/^#/!s/^127.0.0.1[[:space:]]//g' | sed '/^#/!s/$[[:space:]]//g' | sed '/^#/!s/^/local-zone: "/g' | sed '/^#/!s/$/" always_nxdomain/g' > "$(date +%Y-%m-%d_%H:%M:%S_)"blocklist.conf
 # Example filename: 2021-08-01_08:00:00_blocklist.conf
 
 function checkErrors() {
@@ -39,24 +45,40 @@ function checkErrors() {
 	# Append .OK if it passed and .FAIL if checkerrors.out was not empty.
 	if (file checkerrors.out | grep -qx "^checkerrors.out: empty$") ; then
 		for file in ./*blocklist.conf; do
-			touch ~/"$file".OK
+			touch /home/"$UID1000"/"$file".OK
 		done
 	else
 		for file in ./*blocklist.conf; do
-			touch ~/"$file".FAIL && mv "$file" -t ~/
-			(echo "$file failed formatting check. Quitting." && exit)
+			mv checkerrors.out /home/"$UID1000"/"$file".FAIL && mv "$file" /home/"$UID1000"/
 		done
+		echo "$file failed formatting check. Quitting."
+		exit
 	fi
 }
 
 checkErrors
 
-for file in ./*blocklist.conf; do
-	mv "$file" -t ~/
-	# Replace the above line with this line to automatically install via root cron
-	#cp "$file" /etc/unbound/unbound.conf.d/
-done
+if [ "${EUID}" -ne 0 ]; then
+	for file in ./*blocklist.conf; do
+		# Run as normal user, save conf file to $HOME for manual review
+		mv "$file" /home/"$UID1000"/
+	done
+else
+	for file in ./*blocklist.conf; do
+		# Automatically install the updated conf via cron, or by running manually as root
+		if [ -e '/etc/unbound/unbound.conf.d/blocklist.conf' ]; then
+			rm /etc/unbound/unbound.conf.d/blocklist.conf
+		fi
+		cp "$file" /etc/unbound/unbound.conf.d/blocklist.conf
+		if ! (unbound-checkconf | grep 'no errors'); then
+			echo "Error with unbound configuration. Quitting."
+			exit 1
+		fi
+		systemctl restart unbound
+	done
+fi
 
+cd ~/ || exit
 rm -rf "$UPDATE_DIR"
 
 exit
