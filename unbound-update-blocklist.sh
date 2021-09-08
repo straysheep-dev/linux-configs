f8e0ad68059095791605a4e70baea79cc9a6887a
straysheep-dev <84547874+straysheep-dev@users.noreply.github.com>
2021-09-08 02:34:09 -0400

    Move logging to /var/log
    Added logging mechanism to output logs to /var/log/
    Improved conf file format checking

--- unbound-update-blocklist.sh
+++ unbound-update-blocklist.sh
@@ -7,13 +7,39 @@
 # 0 */12 * * * /bin/bash /opt/unbound-update-blocklist.sh
 # Do not run more often than once every 5 minutes: <https://urlhaus.abuse.ch/api/#tos>
 
-UID1000=$(grep '1000' /etc/passwd | cut -d ':' -f 1)
+LOGFILE='unbound-update-blocklist'
 
-UPDATE_DIR=$(mktemp -d)
+# Root check
+if [ "${EUID}" -ne 0 ]; then
+	"This script must be run as root. Quitting."
+	exit 1
+fi
+
+# Create the logging configuration if it doesn't exist
+if ! [ -e /etc/logrotate.d/"$LOGFILE" ]; then
+	echo "# Logging configuration for $LOGFILE shell script
+/var/log/$LOGFILE {
+	nocompress
+	monthly
+	rotate 4
+	missingok
+	notifempty
+	su root root
+	create 0644 root root
+}" > /etc/logrotate.d/"$LOGFILE"
+fi
+
+
+if [ -d /tmp/tmp.updateblocklist ]; then
+	rm -rf /tmp/tmp.updateblocklist
+fi
+
+mkdir /tmp/tmp.updateblocklist
+UPDATE_DIR='/tmp/tmp.updateblocklist'
 export UPDATE_DIR
 
 # Make a temporary working directory to work with these files alone
-cd "$UPDATE_DIR" || (echo "Failed to make temporary directory. Quitting." && touch /home/"$UID1000"/directory-creation.error && exit)
+cd "$UPDATE_DIR" || (echo "Failed creating temporary directory. Quitting." && echo "# [WARNING]: $(date +%Y-%m-%d) $(date +%H:%M:%S) Failed creating temporary directory." >> /var/log/"$LOGFILE" && exit)
 
 # Download blocklists
 curl -sS 'https://urlhaus.abuse.ch/downloads/hostfile/' > 'urlhaus.txt'
@@ -22,65 +48,75 @@ curl -sSLf 'https://raw.githubusercontent.com/StevenBlack/hosts/master/data/yoyo
 # Make sure download was successful and filetype matches what's expected
 for list in ./*.txt; do
 	if ! (file "$list" | grep -Eqx "^./.*\.txt:.*(ASCII text|ASCII text, with CRLF line terminators)$"); then
-		(echo "One or more lists not plain text. Quitting." && touch /home/"$UID1000"/blocklist-download.error && cd ~/ && rm -rf "$UPDATE_DIR")
+		echo 'One or more lists not plain text. Quitting.' 
+		echo "# [WARNING]: $(date +%Y-%m-%d) $(date +%H:%M:%S) One or more lists not plain text." >> /var/log/"$LOGFILE" 
+		cd ~/ || exit
+		rm -rf "$UPDATE_DIR"
 		exit
 	fi
 done
 
+# Check for any lines not beginning with '#', 0.0.0.0, 127.0.0.1, or empty
+# Do this check before parsing the file
+grep -vE "^(#|0\.0\.0\.0|127\.0\.0\.1|^$)" ./*.txt >> checkerrors.out
+
 # Parse hosts files to unbound.conf format; handles removing CRLF line terminators and empty lines
-cat ./*.txt | sed '/^$/d' | sed 's/\r//g' | sed '/^#/!s/^0.0.0.0[[:space:]]//g' | sed '/^#/!s/^127.0.0.1[[:space:]]//g' | sed '/^#/!s/$[[:space:]]//g' | sed '/^#/!s/^/local-zone: "/g' | sed '/^#/!s/$/" always_nxdomain/g' > "$(date +%Y-%m-%d_%H:%M:%S_)"blocklist.conf
-# Example filename: 2021-08-01_08:00:00_blocklist.conf
-# We use for loops to match the filename because it's based on an exact time
+cat ./*.txt | sed '/^$/d' | sed 's/\r//g' | sed '/^#/!s/^0.0.0.0[[:space:]]//g' | sed '/^#/!s/^127.0.0.1[[:space:]]//g' | sed '/^#/!s/$[[:space:]]//g' | sed '/^#/!s/^/local-zone: "/g' | sed '/^#/!s/$/" always_nxdomain/g' > blocklist.conf
 
-function checkErrors() {
+function checkFormatting() {
 	# Here we want to make sure every line is properly formatted
-	# by checking from the start of every line up to the url, and
-	# from the end of every url to the end of line, taking care to
-	# not parse urls directly
-	for file in ./*blocklist.conf; do
-		grep -v "^local-zone: \"" "$file" | grep -v "^#" > checkerrors.out
-		grep -v "\" always_nxdomain$" "$file" | grep -v "^#" >> checkerrors.out
-	done
+	# Checking from the start of every line up to the url, and from the end of every url to the end of line.
+	# We don't parse potentially malicious url's directly
 
-	# If every line passed the previous check, the checkerrors.out file should be empty.
-	# Append .OK if it passed and .FAIL if checkerrors.out was not empty.
-	if (file checkerrors.out | grep -qx "^checkerrors.out: empty$") ; then
-		for file in ./*blocklist.conf; do
-			touch /home/"$UID1000"/"$file".OK
-		done
-	else
-		for file in ./*blocklist.conf; do
-			# file.FAIL contains the lines that did not pass the formatting check
-			mv checkerrors.out /home/"$UID1000"/"$file".FAIL && mv "$file" /home/"$UID1000"/
-		done
-		echo "$file failed formatting check. Quitting."
+	# Use for loops instead of ls grep, if using wildcard * filename matching in the future
+	grep -v "^local-zone: \"" ./blocklist.conf | grep -v "^#" >> checkerrors.out
+	grep -v "\" always_nxdomain$" ./blocklist.conf | grep -v "^#" >> checkerrors.out
+
+	# Append a failure summary to log file if checkerrors.out was not empty.
+	# checkerrors.out contains the lines that did not pass the formatting check
+	if ! (file checkerrors.out | grep -qx "^checkerrors.out: empty$") ; then
+		{
+			echo "# [WARNING]: $(date +%Y-%m-%d) $(date +%H:%M:%S) failed formatting check."
+			echo '==================================================================================='
+			echo '################################ BEGIN ERRORS #####################################'
+			echo ''
+			cat checkerrors.out
+			echo ''
+			echo '################################# END ERRORS ######################################'
+			echo '==================================================================================='
+		} >> /var/log/"$LOGFILE"
+
+		echo 'Failed formatting check. Quitting.'
+		cd ~/ || exit
+		rm -rf "$UPDATE_DIR"
 		exit
 	fi
 }
 
-checkErrors
+checkFormatting
 
-if [ "${EUID}" -ne 0 ]; then
-	for file in ./*blocklist.conf; do
-		# Run as normal user, save conf file to $HOME for manual review
-		mv "$file" /home/"$UID1000"/
-	done
-else
-	for file in ./*blocklist.conf; do
-		# Automatically install the updated conf via cron, or by running manually as root
-		if [ -e '/etc/unbound/unbound.conf.d/blocklist.conf' ]; then
-			mv /etc/unbound/unbound.conf.d/blocklist.conf /tmp/blocklist.conf.bkup
-		fi
-		cp "$file" /etc/unbound/unbound.conf.d/blocklist.conf
-		if ! (unbound-checkconf | grep 'no errors'); then
-			echo "Error with unbound configuration. Quitting."
-			mv /tmp/blocklist.conf.bkup /etc/unbound/unbound.conf.d/blocklist.conf
-			exit 1
-		fi
-		systemctl restart unbound
-		rm /tmp/blocklist.conf.bkup
-	done
+# Automatically install the updated conf via cron, or by running manually
+# Backup current blocklist and check new list to be installed for any additional errors
+if [ -e '/etc/unbound/unbound.conf.d/blocklist.conf' ]; then
+	mv /etc/unbound/unbound.conf.d/blocklist.conf /tmp/blocklist.conf.bkup
 fi
+cp ./blocklist.conf /etc/unbound/unbound.conf.d/blocklist.conf
+if ! (unbound-checkconf | grep 'no errors'); then
+	echo 'Error loading new blocklist, previous configuration restored. Quitting'
+	echo "# [WARNING]: $(date +%Y-%m-%d) $(date +%H:%M:%S) Error loading new blocklist, previous configuration restored." >> /var/log/"$LOGFILE"
+	mv /tmp/blocklist.conf.bkup /etc/unbound/unbound.conf.d/blocklist.conf
+	cd ~/ || exit
+	rm -rf "$UPDATE_DIR"
+	exit 1
+fi
+
+systemctl restart unbound
+if [ -e /tmp/blocklist.conf.bkup ]; then
+	rm /tmp/blocklist.conf.bkup
+fi
+
+# Log successful installation of updated blocklist
+echo "[OK]: $(date +%Y-%m-%d) $(date +%H:%M:%S) blocklist.conf" >> /var/log/"$LOGFILE"
 
 cd ~/ || exit
 rm -rf "$UPDATE_DIR"
