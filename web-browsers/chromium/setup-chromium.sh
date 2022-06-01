a333d63374751398b09d1eb44d2757949abd0c32
straysheep-dev <bah@straysheep.dev>
2022-05-31 21:59:25 -0700

    Update setup-chromium.sh
    - Use sudo instead of running as root
    - Add install function for both Chromium and Chrome

--- web-browsers/chromium/setup-chromium.sh
+++ web-browsers/chromium/setup-chromium.sh
@@ -3,32 +3,114 @@
 # Set "EnableMediaRouter": false, then add the following to stop mdns listener:
 # chrome://flags => enable-webrtc-hide-local-ips-with-mdns => false
 
+BLUE="\033[01;34m"   # information
+GREEN="\033[01;32m"  # information
+YELLOW="\033[01;33m" # warnings
+RED="\033[01;31m"    # errors
+BOLD="\033[01;01m"   # highlight
+RESET="\033[00m"     # reset
+
+
 function isRoot() {
-        if [ "${EUID}" -ne 0 ]; then
-                echo "You need to run this script as root"
+        if [ "${EUID}" -eq 0 ]; then
+                echo "Run this script as a normal user. Quitting."
                 exit 1
         fi
 }
 
 isRoot
 
-function setupChromium() {
-        mkdir -p /etc/chromium-browser/policies/managed
-        mkdir -p /etc/chromium-browser/policies/recommended
+function SetupPolicies() {
+        sudo mkdir -p /etc/chromium-browser/policies/managed
+        sudo mkdir -p /etc/chromium-browser/policies/recommended
 
         if [ -e ./chromium-policies.json ]; then
-		cat ./chromium-policies.json  > /etc/chromium-browser/policies/managed/policies.json
+        	echo -e "[${BLUE}>${RESET}]Installing policies from ./chromium-policies.json..."
+		sudo tee /etc/chromium-browser/policies/managed/policies.json < ./chromium-policies.json > /dev/null
 	else
-		curl -Lf 'https://raw.githubusercontent.com/straysheep-dev/linux-configs/main/web-browsers/chromium/chromium-policies.json' > /etc/chromium-browser/policies/managed/policies.json
+        	echo -e "[${BLUE}>${RESET}]Downloading chromium-policies.json..."
+		curl -Lf 'https://raw.githubusercontent.com/straysheep-dev/linux-configs/main/web-browsers/chromium/chromium-policies.json' | sudo tee /etc/chromium-browser/policies/managed/policies.json > /dev/null
 		if (sha256sum /etc/chromium-browser/policies/managed/policies.json | grep -qx '518c8dbf3477f0264902dccb3e92e98306b9ec8ffb664277128cc7336acf4c10  /etc/chromium-browser/policies/managed/policies.json'); then
-			echo "OK"
+			echo -e "${GREEN}[OK]${RESET}"
 		else
-			echo '[!]Bad signature for policies.json'
+			echo -e "${RED}[\!]Bad signature for policies.json${RESET}"
 		fi
 	fi
 
 	# https://support.google.com/chrome/a/answer/9027408?hl=en
-	ln -s /etc/chromium-browser /etc/opt/chrome
+	if [ -d /etc/opt/chrome ]; then
+		sudo mkdir -p /etc/opt/chrome/policies/managed
+		sudo mkdir -p /etc/opt/chrome/policies/recommended
+		sudo tee /etc/opt/chrome/policies/managed/policies.json < /etc/chromium-browser/policies/managed/policies.json > /dev/null
+	else
+		sudo ln -s /etc/chromium-browser /etc/opt/chrome
+	fi
 }
 
-setupChromium
+function InstallBrowser() {
+	echo ""
+	echo "Do you want to install the browser?"
+	echo ""
+	until [[ $INSTALL_CHOICE =~ ^(y|n)$ ]]; do
+		read -rp "Selection: " -e -i n INSTALL_CHOICE
+	done
+	if [ "$INSTALL_CHOICE" == 'n' ]; then
+		exit 1
+	fi
+
+	echo ""
+	echo "  1. Chromium snap package"
+	echo "  2. Chrome using official Google repo"
+	echo ""
+	until [[ $BROWSER_CHOICE =~ ^(1|2)$ ]]; do
+		read -rp "Selection: " BROWSER_CHOICE
+	done
+	if [ "$BROWSER_CHOICE" == '1' ]; then
+		echo -e "[${BLUE}>${RESET}]Installing Chromium snap package..."
+		sudo apt update && \
+		sudo apt install -y snapd
+		sudo snap install chromium
+	elif [ "$BROWSER_CHOICE" == '2' ]; then
+
+		echo -e "[${BLUE}>${RESET}]Installing Chrome official build..."
+
+		# The deb installer for Chrome creates the following files:
+
+		if ! [ -e /etc/apt/sources.list.d/google-chrome.list ]; then
+			echo "### THIS FILE IS AUTOMATICALLY CONFIGURED ###
+			# You may comment out this entry, but any other modifications may be lost.
+			deb [arch=$(dpkg --print-architecture)] https://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list > /dev/null
+		fi
+		if ! [ -e /etc/apt/trusted.gpg.d/google-chrome.gpg ]; then
+
+			# /etc/apt/trusted.gpg.d/google-chrome.gpg
+			# ----------------------------------------
+			# pub   rsa4096 2016-04-12 [SC]
+			#       EB4C 1BFD 4F04 2F6D DDCC  EC91 7721 F63B D38B 4796
+			# uid           [ unknown] Google Inc. (Linux Packages Signing Authority) <linux-packages-keymaster@google.com>
+			# sub   rsa4096 2019-07-22 [S] [expires: 2022-07-21]
+			# sub   rsa4096 2021-10-26 [S] [expires: 2024-10-25]
+
+
+			# Method 1 from: https://www.google.com/linuxrepositories/
+			#wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+			# Method 2 from: https://github.com/Sysinternals/SysmonForLinux/blob/main/INSTALL.md
+			wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor > google-chrome.gpg
+			sudo mv google-chrome.gpg /etc/apt/trusted.gpg.d/
+			sudo chown root:root /etc/apt/trusted.gpg.d/google-chrome.gpg
+			sudo chmod 644 /etc/apt/trusted.gpg.d/google-chrome.gpg
+
+			if ! (gpg /etc/apt/trusted.gpg.d/google-chrome.gpg | grep '4CCA 1EAF 950C EE4A B839  76DC A040 830F 7FAC 5991'); then echo -e "${RED}BAD SIGNATURE${RESET}"; exit; else echo -e "${GREEN}OK${RESET}"; fi
+			if ! (gpg /etc/apt/trusted.gpg.d/google-chrome.gpg | grep 'EB4C 1BFD 4F04 2F6D DDCC  EC91 7721 F63B D38B 4796'); then echo -e "${RED}BAD SIGNATURE${RESET}"; exit; else echo -e "${GREEN}OK${RESET}"; fi
+		fi
+
+		sudo apt update && \
+		sudo apt install -y google-chrome-stable
+	fi
+}
+
+SetupPolicies
+InstallBrowser
+
+echo -e "[${BLUE}✓${RESET}]Done."
