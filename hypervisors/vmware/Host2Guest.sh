f7501e9779a5d5479025ca3782f4c94c0fc71616
straysheep-dev <bah@straysheep.dev>
2022-09-27 00:06:05 -0700

    Add Host2Guest and Guest2Host scripts

---
+++ hypervisors/vmware/Host2Guest.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Quicker way to run this command.
+# Place this script in /opt
+# sudo ln -s /opt/.../Host2Guest.sh /usr/local/bin/Host2Guest
+
+VM="$(find $HOME -type f -name "$1".vmx)"
+SRC="$2"
+DST="$3"
+
+if [[ "$VM" == '' ]] ; then
+        echo "Enter the name of the VM (.vmx file) as an argument"
+elif [[ "$2" == '' ]]; then
+	echo "Enter the source file as argument 2."
+elif [[ "$3" == '' ]]; then
+	echo "Enter the destination file as argument 3."
+else
+        echo "$VM found."
+	echo ''
+	if (command -v vmrun > /dev/null); then
+		echo "[i] Start Time: $(date +%Y%m%d.%T)"
+		vmrun -T ws CopyFileFromHostToGuest "$VM" "$SRC" "$DST"
+		echo "[i] End Time: $(date +%Y%m%d.%T)"
+	else
+		echo "Missing vmrun command. Quitting."
+		exit 1
+	fi
+fi
