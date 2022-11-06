3964f489037d64c4854e8ea6e467788402f61433
straysheep-dev <bah@straysheep.dev>
2022-11-05 23:18:27 -0700

    Update get-connections

--- get-connections.sh
+++ get-connections.sh
@@ -6,7 +6,7 @@ function GetSSHConnections() {
 	echo "SSH Connections:"
 	echo ""
 	for log in /var/log/auth.log*; do
-		if (sudo file "$log" | grep -P "ASCII text" > /dev/null); then
+		if (sudo file "$log" | grep -P "ASCII text(, with very long lines)?$" > /dev/null); then
 			echo "$log:"
 			sudo grep -F 'Accepted' "$log" | sed 's/Accepted/\nAccepted/g' | grep 'Accepted' | sort | uniq -c | sort -n -r
 		fi
@@ -20,12 +20,16 @@ GetSSHConnections
 # For example:
 # iptables -I INPUT -i ${SERVER_PUB_NIC} -p udp --dport ${SERVER_VPN_PORT} -m state --state ESTABLISHED -j LOG --log-prefix 'Wireguard Connection: '
 # This is useful to learn patterns about client connections to better detect anomalies
-
 function GetVPNConnections() {
 	echo "=================================================="
 	echo "VPN Connections:"
 	echo ""
-	sudo grep 'Connection' /var/log/kern.log | sed 's/SRC=/\nSRC=/g' | grep 'SRC=' | cut -d ' ' -f 1 | sort | uniq -c | sort -n -r
+	for log in /var/log/kern.log*; do
+		if (sudo file "$log" | grep -P "ASCII text(, with very long lines)?$" > /dev/null); then
+			echo "$log:"
+			sudo grep 'Connection' "$log" | sed 's/SRC=/\nSRC=/g' | grep 'SRC=' | cut -d ' ' -f 1 | sort | uniq -c | sort -n -r
+		fi
+	done
 }
 
 GetVPNConnections
