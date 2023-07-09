86a74b545081412c0e2aa68b91347afd96785d24
straysheep-dev <bah@straysheep.dev>
2023-07-08 21:32:27 -0700

    Fix errors in setup-chromium

--- web-browsers/chromium/setup-chromium.sh
+++ web-browsers/chromium/setup-chromium.sh
@@ -85,8 +85,8 @@ function InstallBrowser() {
 
 		if ! [ -e /etc/apt/sources.list.d/google-chrome.list ]; then
 			echo "### THIS FILE IS AUTOMATICALLY CONFIGURED ###
-			# You may comment out this entry, but any other modifications may be lost.
-			deb [arch=$(dpkg --print-architecture)] https://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list > /dev/null
+# You may comment out this entry, but any other modifications may be lost.
+deb [arch=$(dpkg --print-architecture)] https://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list > /dev/null
 		fi
 		if ! [ -e /etc/apt/trusted.gpg.d/google-chrome.gpg ]; then
 
@@ -111,9 +111,8 @@ function InstallBrowser() {
 			# Obtaining the public key directly from a keyserver has the latest signature data without the older dsa key:
 			# https://keyserver.ubuntu.com/pks/lookup?search=EB4C1BFD4F042F6DDDCCEC917721F63BD38B4796&fingerprint=on&op=index
 			gpg --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 'EB4C 1BFD 4F04 2F6D DDCC  EC91 7721 F63B D38B 4796'
-			gpg --export 'EB4C 1BFD 4F04 2F6D DDCC  EC91 7721 F63B D38B 4796' > google-chrome.gpg
+			gpg --export 'EB4C 1BFD 4F04 2F6D DDCC  EC91 7721 F63B D38B 4796' | sudo tee /etc/apt/trusted.gpg.d/google-chrome.gpg
 
-			sudo mv google-chrome.gpg /etc/apt/trusted.gpg.d/
 			sudo chown root:root /etc/apt/trusted.gpg.d/google-chrome.gpg
 			sudo chmod 644 /etc/apt/trusted.gpg.d/google-chrome.gpg
 
