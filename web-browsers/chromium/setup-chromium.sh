893c0948ebeb02af6328450b607212cdd50f32cc
straysheep-dev <bah@straysheep.dev>
2023-09-05 12:35:00 -0700

    Update web-browsers/chromium/setup-chromium.sh

--- web-browsers/chromium/setup-chromium.sh
+++ web-browsers/chromium/setup-chromium.sh
@@ -21,21 +21,29 @@ function isRoot() {
 isRoot
 
 function SetupPolicies() {
-	# This always creates the /etc/chromium-browser/ and /etc/opt/chrome directories,
-	# symlinking the existing files over from /etc/chromium-browser/ to /etc/opt/chrome
+	# This creates the /etc/chromium-browser/ and /etc/opt/chrome policy directories
 
 	# https://www.chromium.org/administrators/linux-quick-start/
         sudo mkdir -p /etc/chromium-browser/policies/managed
         sudo mkdir -p /etc/chromium-browser/policies/recommended
 
-	if [ -e /etc/chromium-browser/policies/managed/policies.json ]; then
+	# https://support.google.com/chrome/a/answer/9027408?hl=en
+	sudo mkdir -p /etc/opt/chrome/policies/managed
+	sudo mkdir -p /etc/opt/chrome/policies/recommended
+
+	if [ -e /etc/opt/chrome/policies/managed/policies.json ] && [ -e /etc/chromium-browser/policies/managed/policies.json ]; then
 		echo -e "[${BLUE}i${RESET}]Policy file already installed."
 	elif [ -e ./chromium-policies.json ]; then
 		echo -e "[${BLUE}>${RESET}]Installing policies from ./chromium-policies.json..."
+		sudo tee /etc/opt/chrome/policies/managed/policies.json < ./chromium-policies.json > /dev/null
 		sudo tee /etc/chromium-browser/policies/managed/policies.json < ./chromium-policies.json > /dev/null
 	else
         	echo -e "[${BLUE}>${RESET}]Downloading chromium-policies.json..."
-		curl -Lf 'https://raw.githubusercontent.com/straysheep-dev/linux-configs/main/web-browsers/chromium/chromium-policies.json' | sudo tee /etc/chromium-browser/policies/managed/policies.json > /dev/null
+		# Write curl download to chrome's policy directory
+		curl -Lf 'https://raw.githubusercontent.com/straysheep-dev/linux-configs/main/web-browsers/chromium/chromium-policies.json' | sudo tee /etc/opt/chrome/policies/managed/policies.json > /dev/null
+		# Tee the policy over to chromium's directory
+		sudo tee /etc/chromium-browser/policies/managed/policies.json < /etc/opt/chrome/policies/managed/policies.json > /dev/null
+
 		# Download the latest README with examples
 		echo -e "[${BLUE}>${RESET}]Downloading README.md..."
 		curl -Lf 'https://raw.githubusercontent.com/straysheep-dev/linux-configs/main/web-browsers/chromium/README.md' | sudo tee /etc/chromium-browser/policies/README.md > /dev/null
@@ -45,12 +53,6 @@ function SetupPolicies() {
 		else
 			echo -e "${RED}[\!]Bad signature for policies.json${RESET}"
 		fi
-	fi
-
-	# https://support.google.com/chrome/a/answer/9027408?hl=en
-	sudo mkdir -p /etc/opt/chrome 2>/dev/null
-	if ! [ -e /etc/opt/chrome/policies ]; then
-		sudo ln -s /etc/chromium-browser/policies /etc/opt/chrome
 	fi
 }
 
